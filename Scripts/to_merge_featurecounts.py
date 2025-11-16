9403eadd6049bf7b5e398b2f77e9886c84041a9c
Farha <151407147+captainbioinfo@users.noreply.github.com>
2025-11-17 00:04:22 +0530

    Add files via upload

---
+++ Scripts/to_merge_featurecounts.py
@@ -0,0 +1,17 @@
+#conda install pandas
+import pandas as pd
+import glob
+
+files = glob.glob("*.tabular")
+
+dfs = []
+for f in files:
+    df = pd.read_csv(f, sep="\t")
+    dfs.append(df)
+
+merged = dfs[0]
+for df in dfs[1:]:
+    merged = merged.merge(df, on="Geneid")
+
+merged.to_csv("merged_counts.csv", index=False)
+
